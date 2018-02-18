6be316dc160b44b75a63bf0a569c22d4121ffca9
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-18 14:48:58 -0800

    1. Set initial redux state to lyric array and array position 0. 2. Create userClick function and attach to html. 3. Create store with reducer as an argument. 4. Write test expecting reducer to return intialState. 5. Write reducer to take intialState, action as arugments. 6. Write test expecting reducer to have NEXT_LYRIC action type and arrayPosition of 1. 7. Write reducer to take initalState, NEXT_LYRIC as arguments using switch statement to set state to have updated arrayPosition, else return state. 8. Write renderLyrics function and call on window object on load. 9. Dispatch NEXT_LYRIC action on user click to advance array position in state 10. Subscribe to changes in store using subscribe() with renderLyrics as an argument to render arrayPosition string as it changes with user click.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,58 @@
-const userClick = () => {
-  console.log('click');
+// LYRIC INFO
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+// INITIAL REDUX STATE
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
 }
+
+// REDUCER WILL GO HERE
+const reducer = (state=initialState, action) => {
+	switch (action.type) {
+		case 'NEXT_LYRIC':
+			let newArrayPosition = state.arrayPosition + 1;
+			let newState = {
+				songLyricsArray: state.songLyricsArray,
+				arrayPosition: newArrayPosition,
+			}
+			return newState;
+		default:
+			return state;
+	}
+}
+// JEST TESTS + SETUP WILL GO HERE
+const {expect} =  window;
+
+expect(reducer(initialState, {type: null})).toEqual(initialState);
+
+expect(reducer(initialState, {type:'NEXT_LYRIC'})).toEqual({songLyricsArray: songLyricsArray, arrayPosition: 1
+});
+
+// REDUX STORE
+const { createStore } = Redux;
+const store = createStore(reducer);
+console.log(store.getState());
+
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+  }
+window.onload = function() {
+  renderLyrics();
+}
+
+// CLICK LISTENER
+const userClick = () => {
+  store.dispatch({ type: 'NEXT_LYRIC'} );
+	console.log(store.getState());
+}
+
+//📖SUBSCRIBE TO REDUX STORE 
+store.subscribe(renderLyrics);
